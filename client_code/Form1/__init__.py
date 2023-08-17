03782c0fcb6a15788e928e310ce3b463dd47979d
f.vazquez0617@gmail.com <f.vazquez0617@gmail.com>
2023-08-17 21:59:50 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,5 +11,21 @@ class Form1(Form1Template):
 
   def categorize_button_click(self, **event_args):
     """This method is called when the button is clicked"""
+    try:
+        
+        self.species_label.visible = True
+        self.species_label.text = "Predicting..."
+
+        pet_category = anvil.server.call('predict_pet',
+                                         self.url.txt,
+                                         self.color.txt)
+        
+        color = self.color.txt
+        if pet_category:
+            self.species_label.text = f"The pet is a {pet_category.capitalize()} with color {color.capitalize()}"
+        else:
+            self.species_label.text = "Prediction failed"
+    except Exception as e:
+        self.species_label.text = "Error: " + str(e)
     pass
 
