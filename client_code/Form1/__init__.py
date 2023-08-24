54e63ec44895f8c69ac35217e48d68caaeae04b1
f.vazquez0617@gmail.com <f.vazquez0617@gmail.com>
2023-08-24 23:54:13 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,7 +15,27 @@ class Form1(Form1Template):
                                     self.url.text)
 
     if pet_category:
+      self.image_1.source = self.url.text
+      self.image_1.visible=True
       self.species_label.visible = True
-      self.species_label.text = "The species is a " + self.color.text + " " + pet_category.capitalize()   
+      self.species_label.text = "According to the CNN, the species is a " + pet_category.capitalize()
+      if pet_category == "Cat":
+        self.chatlabel.text = "ChatCat: Cats are domesticated carnivorous mammals known for their playful behavior, independent nature, and strong bond with humans throughout history. Ask me more if you have more questions!"
+      else: 
+        self.chatlabel.text = "ChatCat: Dogs are loyal and social domesticated animals that belong to the Canidae family, characterized by their diverse breeds, wide range of sizes and appearances, and their roles as companions, working animals, and service animals. Ask me more if you have more questions!"
+      self.chatlabel.visible=True
+      self.chat_input.visible=True
+      self.chatbutton.visible=True
+      
+  def chatbutton_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    chat_output = anvil.server.call('ask_question',
+                                      self.chat_input.text)
+    self.chat_output_label.visible = True
+    self.chat_output_label.text = chat_output
+      
     pass
+
+
+
 
