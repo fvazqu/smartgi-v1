c7f24daa5c47e7d881ecb67dfe79c8d439ddc2fe
f.vazquez0617@gmail.com <f.vazquez0617@gmail.com>
2023-09-06 20:41:58 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 import anvil.server
+import anvil.media
 
 class Form1(Form1Template):
   def __init__(self, **properties):
@@ -15,18 +16,25 @@ class Form1(Form1Template):
                                     self.url.text)
 
     if pet_category:
-      # self.image_3.source=?
       self.image_1.source = self.url.text
       self.image_1.visible=True
       self.species_label.visible = True
-      self.species_label.text = "According to the CNN, the species is a " + pet_category.capitalize()
-      if pet_category == "Cat":
-        self.chatlabel.text = "ChatCat: Cats are domesticated carnivorous mammals known for their playful behavior, independent nature, and strong bond with humans throughout history. Ask me more if you have more questions!"
-      else: 
-        self.chatlabel.text = "ChatCat: Dogs are loyal and social domesticated animals that belong to the Canidae family, characterized by their diverse breeds, wide range of sizes and appearances, and their roles as companions, working animals, and service animals. Ask me more if you have more questions!"
+      self.species_label.text = pet_category.capitalize()
+      self.chatlabel.text = "ChatGPT: Ask me more if you have any questions?"
+      # if pet_category == "Cat":
+      #   self.chatlabel.text = "ChatCat: Cats are domesticated carnivorous mammals known for their playful behavior, independent nature, and strong bond with humans throughout history. Ask me more if you have more questions!"
+      # else: 
+      #   self.chatlabel.text = "ChatCat: Dogs are loyal and social domesticated animals that belong to the Canidae family, characterized by their diverse breeds, wide range of sizes and appearances, and their roles as companions, working animals, and service animals. Ask me more if you have more questions!"
       self.chatlabel.visible=True
       self.chat_input.visible=True
       self.chatbutton.visible=True
+      
+    get_image = anvil.server.call('yolo_image',
+                                    self.url.text)
+    if get_image:
+      self.image_3.source=get_image
+      self.image_3.visible=True
+    
       
   def chatbutton_click(self, **event_args):
     """This method is called when the button is clicked"""
@@ -36,6 +44,8 @@ class Form1(Form1Template):
     self.chat_output_label.text = chat_output
       
     pass
+
+
 
 
 
